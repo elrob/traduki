2cfe9d34e8d3eaac3be7dd734b6d16dd0f5c63fa
John Cowie <john.a.cowie@gmail.com>
2015-06-17 10:29:14 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to traduki
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/traduki/core_test.clj
@@ -0,0 +1,7 @@
+(ns traduki.core-test
+  (:require [clojure.test :refer :all]
+            [traduki.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
