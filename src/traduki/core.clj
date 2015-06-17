bc2c0ed773f9262bf79d0b6931b8b8cdb5d1c231
John Cowie <john.a.cowie@gmail.com>
2015-06-17 10:53:16 +0100

    JC & AB | copied over code from objective8

--- src/traduki/core.clj
+++ src/traduki/core.clj
@@ -1,6 +1,50 @@
-(ns traduki.core)
+(ns traduki.core
+  (:require [net.cgrand.enlive-html :as html]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn translator
+  "Returns a translation function which replaces the
+  content of nodes with translations for k"
+  [{:keys [translations] :as context}]
+  (fn [k]
+    #(assoc % :content (translations k))))
+
+(defn text->p-nodes
+  "Turns text into a collection of paragraph nodes based on linebreaks.
+  Returns nil if no text is supplied"
+  [text]
+  (when text
+    (let [newline-followed-by-optional-whitespace #"(\n+|\r+)\s*"]
+      (map (fn [p] (html/html [:p p])) (clojure.string/split text
+                                                             newline-followed-by-optional-whitespace)))))
+
+(defn- apply-translation [translation-string node translations]
+  (let [[translation-type-string translation-key-string] (clojure.string/split translation-string #":")
+        translation-type (keyword translation-type-string)
+        translation-key (keyword translation-key-string)
+        translation (translations translation-key)]
+    (cond
+      (= :content translation-type)
+      (assoc node :content (list translation))
+
+      (= :html translation-type)
+      (assoc node :content (html/html-snippet translation))
+
+      (= "attr" (namespace translation-type))
+      (assoc-in node [:attrs (keyword (name translation-type))] translation)
+
+      :else node)))
+
+(defn- apply-translations [[translation-string & more] node translations]
+  (let [translated-node (apply-translation translation-string node translations)]
+    (if more
+      (recur more translated-node translations)
+      translated-node)))
+
+(defn- translate-node  [node  {:keys  [translations] :as context}]
+  (let [translation-strings (clojure.string/split (get-in node [:attrs :data-l8n]) #"\s+")]
+    (apply-translations translation-strings node translations)))
+
+(defn translate [context nodes]
+  (html/at nodes
+           [(html/attr? :data-l8n)] #(translate-node % context)))
+
---
+++ test/traduki/test/core.clj
@@ -0,0 +1,74 @@
+(ns traduki.test.core
+  (:require [midje.sweet :refer :all]
+            [net.cgrand.enlive-html :as html]
+            [traduki.core :refer :all]))
+
+(facts "Splitting text into paragraphs"
+       (fact "Newlines are turned into paragraphs"
+             (text->p-nodes "Hello\nWorld") =>
+             [[{:tag :p :content '("Hello") :attrs {}}]
+              [{:tag :p :content '("World") :attrs {}}]])
+
+       (fact "Collapses multiple newlines into single paragraphs"
+             (text->p-nodes "Hello\n\n\nWorld\n\r\nFoo\n   \n \n \nBar Zap\r\t\n") =>
+             [[{:tag :p :content '("Hello") :attrs {}}]
+              [{:tag :p :content '("World") :attrs {}}]
+              [{:tag :p :content '("Foo") :attrs {}}]
+              [{:tag :p :content '("Bar Zap") :attrs {}}]])
+
+       (fact "Deals with nil text"
+             (text->p-nodes nil) => nil))
+
+(facts "About translating templates"
+
+       (def translations :mock-translations-function)
+
+       (fact "content is translated"
+             (let [input-html-resource (html/html-resource (java.io.StringReader. "<p data-l8n=\"content:some-key/some-value\">!UNTRANSLATED_CONTENT</p>"))]
+               (-> (translate {:translations translations} input-html-resource)
+                   (html/select [:p])
+                   first
+                   (html/text)) => "TRANSLATED_CONTENT"
+               (provided
+                 (translations :some-key/some-value) => "TRANSLATED_CONTENT")))
+
+       (fact "static html content can be used as a translation"
+             (let [input-html-resource (html/html-resource (java.io.StringReader. "<p data-l8n=\"html:some-key/some-value\">!UNTRANSLATED_NON_HTML_CONTENT</p>"))]
+               (-> (translate {:translations translations} input-html-resource)
+                   (html/select [:p])
+                   first
+                   (html/select [:.static-html-class])
+                   first
+                   (html/text)) => "TRANSLATED_CONTENT_INSIDE_DIV"
+               (provided
+                 (translations :some-key/some-value) => "<div class=\"static-html-class\">TRANSLATED_CONTENT_INSIDE_DIV</div>")))
+
+       (fact "html attributes can be translated"
+             (let [input-html-resource (html/html-resource (java.io.StringReader. "<p data-l8n=\"attr/title:some-key/some-value\" title=\"UNTRANSLATED_TITLE_ATTRIBUTE\">!SOME_CONTENT</p>"))]
+               (-> (translate {:translations translations} input-html-resource)
+                   (html/select [:p])
+                   first
+                   :attrs
+                   :title) => "TRANSLATED_TITLE_ATTRIBUTE"
+               (provided
+                 (translations :some-key/some-value) => "TRANSLATED_TITLE_ATTRIBUTE")))
+
+       (fact "can translate content and attributes in same element"
+             (against-background
+               (translations :attr-key/attr-value) => "TRANSLATED_TITLE_ATTRIBUTE"
+               (translations :content-key/content-value) => "TRANSLATED_CONTENT")
+             (let [data-l8n-string "attr/title:attr-key/attr-value content:content-key/content-value"
+                   input-html-string (str "<p data-l8n=\"" data-l8n-string
+                                          "\" title=\"UNTRANSLATED_TITLE_ATTRIBUTE\">!UNTRANSLATED_CONTENT</p>")
+                   input-html-resource (html/html-resource (java.io.StringReader. input-html-string))
+                   translated-resource (translate {:translations translations} input-html-resource)]
+               (-> translated-resource
+                   (html/select [:p])
+                   first
+                   :attrs
+                   :title) => "TRANSLATED_TITLE_ATTRIBUTE"
+               (-> translated-resource
+                   (html/select [:p])
+                   first
+                   (html/text)) => "TRANSLATED_CONTENT")))
+
